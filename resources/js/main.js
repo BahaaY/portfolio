9cb3c8a39bf09c76846c9e3a2e0d66dc565fd2c9
BahaaY <bahaayassin92@gmail.com>
2023-10-25 18:40:20 +0300

    updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,6 +3,7 @@ function about_me(){
     $("#btn_about_me").addClass("active");
     $("#btn_device_about_me").addClass("active");
     $("#section-about-me").slideDown();
+    $("#device-menu").hide();
 }
 
 function courses(){
@@ -10,47 +11,18 @@ function courses(){
     $("#btn_courses").addClass("active");
     $("#btn_device_courses").addClass("active");
     $("#section-courses").slideDown();
+    $("#device-menu").hide();
 }
 
-function certification(){
+function certifications(){
     remove_class_active();
     $("#btn_certifications").addClass("active");
     $("#btn_device_certifications").addClass("active");
     $("#section-certifications").slideDown();
+    $("#device-menu").hide();
 }
 
 function projects(){
-    remove_class_active();
-    $("#btn_projects").addClass("active");
-    $("#btn_device_projects").addClass("active");
-    $("#section-projects").slideDown();
-}
-
-function device_about_me(){
-    remove_class_active();
-    $("#btn_about_me").addClass("active");
-    $("#btn_device_about_me").addClass("active");
-    $("#section-about-me").slideDown();
-    $("#device-menu").hide();
-}
-
-function device_courses(){
-    remove_class_active();
-    $("#btn_courses").addClass("active");
-    $("#btn_device_courses").addClass("active");
-    $("#section-courses").slideDown();
-    $("#device-menu").hide();
-}
-
-function device_certifications(){
-    remove_class_active();
-    $("#btn_certifications").addClass("active");
-    $("#btn_device_certifications").addClass("active");
-    $("#section-certifications").slideDown();
-    $("#device-menu").hide();
-}
-
-function device_projects(){
     remove_class_active();
     $("#btn_projects").addClass("active");
     $("#btn_device_projects").addClass("active");
@@ -76,5 +48,13 @@ function remove_class_active(){
 }
 
 function open_device_menu(){
-    $("#device-menu").slideDown();
+    $("#device-menu").slideDown(function(){
+        $("#row_close").slideDown();
+    });
+}
+
+function close_menu(){
+    $("#row_close").slideUp(function(){
+        $("#device-menu").slideUp();
+    });
 }
